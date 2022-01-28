d75abfad7e5f5a0c7ce451976c8ae720460b1a9c
woobottle <dkrnfls@gmail.com>
2022-01-28 17:50:39 +0900

    version up

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
+#!/usr/bin/env node
 import { BANNER } from './ui/banner';
 import { program } from 'commander';
 import { CommandLoader } from './commands';
 
 const bootstrap = () => {
   console.log(BANNER)
-  program.version('0.0.1', '-v, --version');
+  program.version('0.0.2', '-v, --version');
   CommandLoader.load(program);
   program.parse(process.argv);
 
